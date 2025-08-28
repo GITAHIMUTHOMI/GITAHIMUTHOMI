89174f6d897466cef4d90e13e48dd28beef6dd70
Patrick Gitahi <gitahimuthomi@gmail.com>
2025-08-28 16:26:14 +0300

    Add calculator.py

---
+++ PLP_Intro.py
@@ -0,0 +1,21 @@
+#PLP="Academy"
+# PLP="Academy"
+# Student_Name="Gitahi Muthomi"
+# Student_ID="1001"
+# Student_Age="20"
+# Course_Name="Python Programming"
+# Fees_clearance="True"
+# #output
+# print(Student_Name, Student_ID, Student_Age, Fees_clearance)
+#Lists
+Languages=("1995", "Python","Java", "C++")
+print(Languages)
+#Tuples
+products=('XBOX, 49.99, "Habibi", 23)
+print(products)
+#Sets
+Set_A={1,2,3,4,5}
+Set_B={4,5,6,7,8}
+print(Set_A)
+Print(Set_B)
+#Dictionaries
